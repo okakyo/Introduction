176a3b19e070c7ed4cd05c84faa431043fc22049
okakyo <0622okakyo@gmail.com>
2019-06-07 18:13:00 +0900

    PWA対応

--- tabs.js
+++ tabs.js
@@ -1,6 +1,11 @@
-$(document).ready(function(){
-    $('ul.tabs').tabs({
-      swipeable : true,
-      responsiveThreshold : 1920
-    });
-  });
+$('a[href^="#"]').click(function() {
+  // スクロールの速度
+  var speed = 400; // ミリ秒で記述
+  var href = $(this).attr("href");
+  var target = $(href == "#" || href == "" ? 'html' : href);
+  var position = target.offset().top;
+  $('body,html').animate({
+    scrollTop: position
+  }, speed, 'swing');
+  return false;
+});
